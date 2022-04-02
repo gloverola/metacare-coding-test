f6e93d375786699bed3d23ea5c3972c1045a6a3a
Glover Olaoluwa <glovermartial@gmail.com>
2022-04-02 10:43:08 +0100

    feat: bootstrapped project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { ReactComponent as TextIcon } from "assets/admin-icon.svg";
 
 const App = () => {
-  return <div>Metacare</div>;
+  return (
+    <div>
+      Metacare <TextIcon />
+    </div>
+  );
 };
 
 export default App;
